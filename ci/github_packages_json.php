29b1bdd09d7fcc4fbc1563c2929e38c32295adfb
Tomas Votruba <tomas.vot@gmail.com>
2020-11-18 13:58:01 +0000

    run solelyl

--- ci/github_packages_json.php
+++ ci/github_packages_json.php
@@ -2,27 +2,30 @@
 
 declare(strict_types=1);
 
-// get top 10 packages names
-$page = (int) ($argv[1] ?? 1);
+// get top page count x10 packages names
+$pageCount = 5;
 
 $packagistPackageProvider = new PackagistPackageProvider();
-$packagistPackageProvider->providePackageJsonListForPage($page);
+$packagistPackageProvider->providePackageJsonListForPage($pageCount);
 
 final class PackagistPackageProvider
 {
-    public function providePackageJsonListForPage(int $page): void
+    public function providePackageJsonListForPage(int $pageCount): void
     {
-        $json = file_get_contents('https://packagist.org/explore/popular.json?page=' . $page);
-        $array = json_decode($json, true);
-
         $packageNames = [];
-        foreach ($array['packages'] as $package) {
-            if ($this->isMetaPackage($package['name'])) {
-                continue;
+
+        for ($i = 1; $i <= $pageCount; ++$i) {
+            $json = file_get_contents('https://packagist.org/explore/popular.json?page=' . $i);
+            $array = json_decode($json, true);
+
+            foreach ($array['packages'] as $package) {
+                if ($this->isMetaPackage($package['name'])) {
+                    continue;
+                }
+
+
+                $packageNames[] = $package['name'];
             }
-
-
-            $packageNames[] = $package['name'];
         }
 
         echo json_encode(['package_name' => $packageNames]);
